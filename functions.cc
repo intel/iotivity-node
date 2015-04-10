476e9f2bf748645a1411f10cae413335b158c5aa
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-04-10 18:09:10 +0300

    CreateNewOptionNode: Start binding

--- functions.cc
+++ functions.cc
@@ -35,7 +35,44 @@ void bind_CoAPToOCResponseCode( const FunctionCallbackInfo<Value>& args ) {
 			( double )CoAPToOCResponseCode( ( uint8_t )args[ 0 ]->Uint32Value() ) ) );
 }
 
+void bind_CreateNewOptionNode( const FunctionCallbackInfo<Value>& args ) {
+	Isolate* isolate = Isolate::GetCurrent();
+
+	if ( args.Length() < 3 ) {
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "Need key, length, and data" ) ) );
+		return;
+	}
+
+	if ( !args[ 0 ]->IsNumber() ) {
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "key must be a number" ) ) );
+		return;
+	}
+
+	if ( !args[ 1 ]->IsNumber() ) {
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "length must be a number" ) ) );
+		return;
+	}
+
+	if ( !args[ 2 ]->IsArray() ) {
+		isolate->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( isolate, "key must be an array" ) ) );
+		return;
+	}
+
+	args.GetReturnValue().Set(
+		Buffer::New( isolate,
+			CreateNewOptionNode(
+				( unsigned short )args[ 0 ]->ToUint32(),
+				( unsigned int )args[ 1 ]->ToUint32(),
+				Buffer::Data( args[ 2 ]->ToObject() ) ),
+			length ) );
+}
+
 void InitFunctions( Handle<Object> exports ) {
 	NODE_SET_METHOD( exports, "hello", bind_hello );
 	NODE_SET_METHOD( exports, "CoAPToOCResponseCode", bind_CoAPToOCResponseCode );
+	NODE_SET_METHOD( exports, "CreateNewOptionNode", bind_CreateNewOptionNode );
 }
