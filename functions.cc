a60b3c0fd10e2e5e1a7617f3856b74689d9a050d
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-04-20 13:05:57 +0300

    Functions: Bind FormOCResponse()

--- functions.cc
+++ functions.cc
@@ -80,9 +80,44 @@ void bind_FormOCClientResponse( const FunctionCallbackInfo<Value>& args ) {
 				( const unsigned char * )*String::Utf8Value( args[ 4 ] ) ) ) );
 }
 
+void bind_FormOCResponse( const FunctionCallbackInfo<Value>& args ) {
+	Isolate *isolate = Isolate::GetCurrent();
+	OCResponse *response = 0;
+	OCStackResult returnValue;
+	Handle<Array> returnArray;
+
+	// We assume that the first argument of the C function, OCResponse **responseLoc, is really
+	// a return value. Thus, this function will return an array of two elements where the first is
+	// the return value of the C function, and the second is a Buffer containing the data received
+	// via the first argument of the C function - if any.
+
+	VALIDATE_ARGUMENT_COUNT( isolate, args, 3 );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 0, IsObject );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 1, IsUint32 );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 2, IsObject );
+
+	// Make native call
+	returnValue = FormOCResponse(
+		&response,
+		( ClientCB* )Buffer::Data( args[ 0 ]->ToObject() ),
+		( uint8_t )args[ 1 ]->Uint32Value(),
+		( OCClientResponse* )Buffer::Data( args[ 2 ]->ToObject() ) );
+
+	// Create the response array - length is 1 if response is NULL, otherwise we pass a reference
+	// to the data stored in response in the second element
+	returnArray = Array::New( isolate, response ? 2 : 1 );
+	returnArray->Set( 0, Number::New( isolate, ( double )returnValue ) );
+	if ( response ) {
+		returnArray->Set( 1, Buffer::Use( isolate, (char *)response, sizeof( OCResponse ) ) );
+	}
+
+	args.GetReturnValue().Set( returnArray );
+}
+
 void InitFunctions( Handle<Object> exports ) {
 	NODE_SET_METHOD( exports, "hello", bind_hello );
 	NODE_SET_METHOD( exports, "CoAPToOCResponseCode", bind_CoAPToOCResponseCode );
 	NODE_SET_METHOD( exports, "CreateNewOptionNode", bind_CreateNewOptionNode );
 	NODE_SET_METHOD( exports, "FormOCClientResponse", bind_FormOCClientResponse );
+	NODE_SET_METHOD( exports, "FormOCResponse", bind_FormOCResponse );
 }
