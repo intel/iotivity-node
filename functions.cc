412a950c6fb0880bd7e1d4f63ff60396ff921ff6
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-04-22 13:22:00 +0300

    Functions: Bind OCCreateResource

--- functions.cc
+++ functions.cc
@@ -2,6 +2,22 @@
 #include <node_buffer.h>
 #include <v8.h>
 
+extern "C" {
+#include <ocstack.h>
+#include <ffi.h>
+#include "callback-info.h"
+}
+
+#define THROW_TYPECHECK_EXCEPTION( isolate, typecheck, message ) \
+		(isolate)->ThrowException( Exception::TypeError( \
+			String::NewFromUtf8( (isolate), \
+				message " must satisfy " #typecheck "()" ) ) )
+
+#define VALIDATE_CALLBACK_RETURN_VALUE_TYPE( isolate, value, typecheck ) \
+	if ( !value->typecheck() ) { \
+		THROW_TYPECHECK_EXCEPTION( isolate, typecheck, "Callback return value type" ); \
+	}
+
 #define VALIDATE_ARGUMENT_COUNT( isolate, args, length ) \
 	if ( (args).Length() < (length) ) { \
 		(isolate)->ThrowException( Exception::TypeError( \
@@ -11,19 +27,14 @@
 
 #define VALIDATE_ARGUMENT_TYPE( isolate, args, index, typecheck  ) \
 	if ( !(args)[ (index) ]->typecheck() ) { \
-		(isolate)->ThrowException( Exception::TypeError( \
-			String::NewFromUtf8( (isolate), "Argument " #index " must satisfy " #typecheck "()" ) ) ); \
+		THROW_TYPECHECK_EXCEPTION( isolate, typecheck, "Argument " #index " " ); \
 		return; \
 	}
 
-extern "C" {
-
-#include <ocstack.h>
-
-}
-
 using namespace v8;
 using namespace node;
+
+Handle<Object> module_exports;
 
 void bind_OCInit( const FunctionCallbackInfo<Value>& args ) {
 	Isolate *isolate = Isolate::GetCurrent();
@@ -46,7 +57,127 @@ void bind_OCStop( const FunctionCallbackInfo<Value>& args ) {
 	args.GetReturnValue().Set( Number::New( isolate, OCStop() ) );
 }
 
+inline Local<Value> get_js_callback( Isolate *isolate, int uuid ) {
+	return module_exports->
+		Get( String::NewFromUtf8( isolate, "_callbacks" ) )->ToObject()->
+			Get( String::NewFromUtf8( isolate, "callbacks" ) )->ToObject()->
+				Get( uuid );
+}
+
+void dispose_of_callback_info( const v8::WeakCallbackData<v8::Value, _callback_info>& data ) {
+	callback_info_free( ( callback_info * )data.GetParameter() );
+}
+
+// Marshaller for OCEntityHandler callback
+// defaultEntityHandler is placed in a closure each time someone calls OCCreateResource. Closures
+// differ from one another only by the value of uuid, which is a key into the hash of JS callbacks.
+// When the C API executes one of the closures, we construct a call to the JS callback identified
+// by its UUID, and pass the return value from the callback back to the C API.
+void defaultEntityHandler(
+		ffi_cif* cif,
+		OCEntityHandlerResult *returnValueLocation,
+		void**arguments,
+		int uuid ) {
+	Isolate *isolate = Isolate::GetCurrent();
+	Local<Value> jsCallback = get_js_callback( isolate, uuid );
+	Local<Value> jsCallbackArguments[ 2 ] = {
+		Number::New( isolate, ( double )*( OCEntityHandlerFlag * )( arguments[ 0 ] ) ),
+		Buffer::Use( isolate, ( char * )*( OCEntityHandlerRequest ** )( arguments[ 1 ] ),
+			sizeof( OCEntityHandlerRequest ) )
+	};
+
+	// Make sure the JS callback is present
+	if ( !jsCallback->IsFunction() ) {
+		isolate->ThrowException( Exception::TypeError(
+			String::Concat(
+				String::Concat(
+					String::NewFromUtf8( isolate, "JS Callback with UUID " ),
+					String::NewFromUtf8( isolate,
+						*String::Utf8Value( Number::New( isolate, ( double )uuid ) ) ) ),
+				String::NewFromUtf8( isolate, " not found" ) ) ) );
+		return;
+	}
+
+	// Call the JS callback
+	Local<Value> returnValue = Local<Function>::Cast( jsCallback )->Call(
+		isolate->GetCurrentContext()->Global(),
+		2, jsCallbackArguments );
+
+	VALIDATE_CALLBACK_RETURN_VALUE_TYPE( isolate, returnValue, IsNumber );
+
+	*returnValueLocation = ( OCEntityHandlerResult )( returnValue->ToNumber()->Value() );
+}
+
+void bind__partial_OCCreateResource( const FunctionCallbackInfo<Value>& args ) {
+	Isolate *isolate = Isolate::GetCurrent();
+	int uuid;
+	OCResourceHandle handle = 0;
+	OCStackResult result;
+	Handle<Array> returnArray;
+	callback_info *info;
+
+	VALIDATE_ARGUMENT_COUNT( isolate, args, 5 );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 0, IsString );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 1, IsString );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 2, IsString );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 3, IsUint32 ); // uuid used as key into callbacks array
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 4, IsUint32 );
+
+	uuid = args[ 3 ]->ToUint32()->Value();
+
+	// Create a new callback
+	info = callback_info_new(
+
+		// Location of JS callback
+		uuid,
+
+		// Function signature - return value
+		&ffi_type_uint32,
+
+		// Function signature - location of function
+		( Marshaller )defaultEntityHandler,
+
+		// Function signature - number of arguments
+		2,
+
+		// Function signature - arguments
+		&ffi_type_uint32, &ffi_type_pointer );
+
+	if ( !info ) {
+		(isolate)->ThrowException( Exception::TypeError(
+			String::NewFromUtf8( (isolate),
+				"OCCreateResource: Unable to allocate C callback" ) ) );
+		return;
+	}
+
+	result = OCCreateResource(
+		&handle,
+		( const char * )*String::Utf8Value( args[ 0 ] ),
+		( const char * )*String::Utf8Value( args[ 1 ] ),
+		( const char * )*String::Utf8Value( args[ 2 ] ),
+		( OCEntityHandler )( info->resultingFunction ),
+		( uint8_t )args[ 4 ]->ToUint32()->Value() );
+
+	// We return an array containing the result code and the handle. It is made weakly
+	// persistent so as to allow us to dispose of the callback_info structure when it's no
+	// longer in use.
+	returnArray = Array::New( isolate, 3 );
+	returnArray->Set( 0, Number::New( isolate, ( double )result ) );
+	returnArray->Set( 1,
+		Buffer::New( isolate, ( const char * )&handle, sizeof( OCResourceHandle ) ) );
+	returnArray->Set( 2, Number::New( isolate, ( double )uuid ) );
+
+	Persistent<Value> jsReturnValue( isolate, returnArray );
+
+	jsReturnValue.SetWeak( info, dispose_of_callback_info );
+
+	args.GetReturnValue().Set( jsReturnValue );
+}
+
 void InitFunctions( Handle<Object> exports ) {
+	module_exports = exports;
+
 	NODE_SET_METHOD( exports, "OCInit", bind_OCInit );
 	NODE_SET_METHOD( exports, "OCStop", bind_OCStop );
+	NODE_SET_METHOD( exports, "_partial_OCCreateResource", bind__partial_OCCreateResource );
 }
--- lowlevel.js
+++ lowlevel.js
@@ -2,3 +2,21 @@
 var iotivity = require( "bindings" )( "iotivity" );
 
 module.exports = iotivity;
+
+iotivity._callbacks = require( "./callbackmanager" );
+
+iotivity.OCCreateResource = function(
+		resourceTypeName,
+		resourceInterfaceName,
+		uri,
+		entityHandler,
+		resourceProperties ) {
+
+	return this._partial_OCCreateResource(
+		resourceTypeName,
+		resourceInterfaceName,
+		uri,
+		this._callbacks.add( entityHandler ),
+		resourceProperties );
+
+};
