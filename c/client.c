c10c24622a2b624e12d89d562a1ea7fc68918e96
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-05-05 15:46:39 +0300

    Client/server study: Use GET request

--- c/client.c
+++ c/client.c
@@ -8,9 +8,10 @@ runOCProcess( gpointer user_data ) {
 	GMainLoop *mainLoop = ( GMainLoop * )user_data;
 
 	if ( result == OC_STACK_OK ) {
+		g_message( "OCProcess() succeeded" );
 		return G_SOURCE_CONTINUE;
 	} else {
-		g_warning( "OCProcess() did not return OC_STACK_OK - quitting main loop\n" );
+		g_warning( "OCProcess() did not return OC_STACK_OK - quitting main loop" );
 		g_main_loop_quit( mainLoop );
 		return G_SOURCE_REMOVE;
 	}
@@ -18,13 +19,13 @@ runOCProcess( gpointer user_data ) {
 
 static OCStackApplicationResult
 defaultCb( void *context, OCDoHandle handle, OCClientResponse *response ) {
-	g_message( "defaultCb() has received a message\n" );
+	g_message( "defaultCb() has received a message" );
 
 	return OC_STACK_DELETE_TRANSACTION;
 }
 
 static void
-addPresenceCallback() {
+addCallback() {
 	OCDoHandle handle;
 	OCCallbackData cbData = {
 		NULL,
@@ -34,8 +35,8 @@ addPresenceCallback() {
 
 	if ( OC_STACK_OK != OCDoResource(
 			&handle,
-			OC_REST_PRESENCE,
-			"coap://255.255.255.255:5683/a/led",
+			OC_REST_GET,
+			"light",
 			NULL,
 			NULL,
 			OC_ALL,
@@ -44,7 +45,9 @@ addPresenceCallback() {
 			NULL,
 			0 ) ) {
 
-		g_warning( "OCDoResource() failed\n" );
+		g_warning( "OCDoResource() failed" );
+	} else {
+		g_message( "OCDoResource() succeeded" );
 	}
 }
 
@@ -53,13 +56,15 @@ main( int argc, char **argv ) {
 	GMainLoop *mainLoop = g_main_loop_new( NULL, FALSE );
 
 	if ( OC_STACK_OK == OCInit( NULL, 0, OC_CLIENT ) ) {
-		addPresenceCallback();
+		g_message( "OCInit() succeeded" );
+
+		addCallback();
 		g_timeout_add_seconds( 2, runOCProcess, mainLoop );
 		g_main_loop_run( mainLoop );
 		OCStop();
 		return 0;
 	}
 
-	g_warning( "OCInit() failed\n" );
+	g_warning( "OCInit() failed" );
 	return 1;
 }
--- c/server.c
+++ c/server.c
@@ -8,28 +8,47 @@ runOCProcess( gpointer user_data ) {
 	GMainLoop *mainLoop = ( GMainLoop * )user_data;
 
 	if ( result == OC_STACK_OK ) {
+		g_message( "OCProcess() succeeded" );
 		return G_SOURCE_CONTINUE;
 	} else {
-		g_warning( "OCProcess() did not return OC_STACK_OK - quitting main loop\n" );
+		g_warning( "OCProcess() did not return OC_STACK_OK - quitting main loop" );
 		g_main_loop_quit( mainLoop );
 		return G_SOURCE_REMOVE;
 	}
 }
 
+static OCEntityHandlerResult
+handleLight1( OCEntityHandlerFlag flag, OCEntityHandlerRequest *request ) {
+	g_message( "handleLight1: Entering" );
+	return OC_EH_OK;
+}
+
 int
 main( int argc, char **argv ) {
 	GMainLoop *mainLoop = g_main_loop_new( NULL, FALSE );
+	OCResourceHandle handle;
 
 	if ( OC_STACK_OK == OCInit( NULL, 0, OC_SERVER ) ) {
-		g_timeout_add_seconds( 2, runOCProcess, mainLoop );
+		g_message( "OCInit() succeeded" );
 
-		OCStartPresence( 0 );
+		if ( OC_STACK_OK == OCCreateResource(
+			&handle,
+			"light",
+			"oc.mi.def",
+			"/light/1",
+			handleLight1,
+			OC_DISCOVERABLE ) ) {
 
-		g_main_loop_run( mainLoop );
-		OCStop();
-		return 0;
+			g_message( "OCCreateResource() succeeded" );
+
+			g_timeout_add_seconds( 2, runOCProcess, mainLoop );
+			OCStartPresence( 0 );
+			g_main_loop_run( mainLoop );
+			OCStop();
+			return 0;
+		}
 	}
 
-	g_warning( "OCInit() failed\n" );
+	g_warning( "Startup failed" );
 	return 1;
 }
