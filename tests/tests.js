76f09081d59d61a372f3a729246dae700ae230d6
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-05-07 14:26:32 +0300

    Tests: Break up startup/shutdown testing

--- tests/tests.js
+++ tests/tests.js
@@ -13,33 +13,44 @@ function lookupEnumValueName( enumeration, value ) {
 	}
 }
 
+function testStartup( assert ) {
+	var result = iotivity.OCInit(
+		null,
+		0,
+		iotivity.OCMode.OC_CLIENT_SERVER );
+
+	assert.deepEqual(
+		lookupEnumValueName(
+			iotivity.OCStackResult,
+			result ),
+		"OC_STACK_OK",
+		"OCInit succeeded" );
+
+	return result;
+}
+
+function testShutdown( assert ) {
+	assert.deepEqual(
+		lookupEnumValueName(
+			iotivity.OCStackResult,
+			iotivity.OCStop() ),
+		"OC_STACK_OK",
+		"OCStop succeeded" );
+}
+
 // Each test requires that we start the stack before we run the test and stop the stack afterwards
 function startStopStack( testBody ) {
-	return function() {
-		QUnit.assert.deepEqual(
-			lookupEnumValueName(
-				iotivity.OCStackResult,
-				iotivity.OCInit(
-					"0.0.0.0",
-					10123,
-					iotivity.OCMode.OC_CLIENT_SERVER ) ),
-			"OC_STACK_OK",
-			"OCInit succeeded" );
-
-		testBody();
-
-		QUnit.assert.deepEqual(
-			lookupEnumValueName(
-				iotivity.OCStackResult,
-				iotivity.OCStop() ),
-			"OC_STACK_OK",
-			"OCStop succeeded" );
+	return function( assert ) {
+		if ( testStartup( assert ) === iotivity.OCStackResult.OC_STACK_OK ) {
+			testBody( assert );
+			testShutdown( assert );
+		}
 	}
 }
 
 test( "Stack start/stop", startStopStack( function() {} ) );
 
-test( "Create/delete resource", startStopStack( function() {
+test( "Create/delete resource", startStopStack( function( assert ) {
 	var result, keyCount,
 		resource = {},
 		callback = function testCallback( flag, request ) {};
@@ -52,25 +63,25 @@ test( "Create/delete resource", startStopStack( function() {
 		"/light/1",
 		callback,
 		iotivity.OCResourceProperty.OC_DISCOVERABLE );
-	QUnit.assert.deepEqual(
+	assert.deepEqual(
 		lookupEnumValueName( iotivity.OCStackResult, result ),
 		"OC_STACK_OK",
 		"OCCreateResource succeeded" );
-	QUnit.assert.deepEqual( typeof resource.uuid, "number", "uuid is present" );
-	QUnit.assert.deepEqual(
+	assert.deepEqual( typeof resource.uuid, "number", "uuid is present" );
+	assert.deepEqual(
 		Object.keys( iotivity._test_callbacks ).length,
 		keyCount + 1,
 		"The number of keys in the list of callbacks has increased by one" );
-	QUnit.assert.deepEqual( iotivity._test_callbacks[ resource.uuid ], callback,
+	assert.deepEqual( iotivity._test_callbacks[ resource.uuid ], callback,
 		"The JS callback is present in the list of callbacks at the correct uuid" );
 
 	keyCount = Object.keys( iotivity._test_callbacks ).length;
 	result = iotivity.OCDeleteResource( resource );
-	QUnit.assert.deepEqual(
+	assert.deepEqual(
 		lookupEnumValueName( iotivity.OCStackResult, result ),
 		"OC_STACK_OK",
 		"OCDeleteResource succeeded" );
-	QUnit.assert.deepEqual(
+	assert.deepEqual(
 		Object.keys( iotivity._test_callbacks ).length,
 		keyCount - 1,
 		"The number of keys in the list of callbacks has decreased by one" );
