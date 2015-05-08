19e00edc291e030c5e91b53ebfd2fa63b2e4145f
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-05-08 13:13:59 +0300

    Tests: Simple discovery bounces against test server it starts

--- tests/tests/simple-discovery.js
+++ tests/tests/simple-discovery.js
@@ -5,48 +5,63 @@ var iotivity = require( "../../index" ),
 
 test( "Simple discovery", function( assert ) {
 	var result, stopProcessing,
+		stopTestServer,
 		done = assert.async(),
 		failsafeTimeoutId = null,
 		handle = {},
 		teardown = function() {
 
+			if ( stopTestServer ) {
+				stopTestServer();
+			}
+
 			if ( stopProcessing ) {
 				stopProcessing();
 			}
+
 			if ( failsafeTimeoutId !== null ) {
 				clearTimeout( failsafeTimeoutId );
 				failsafeTimeoutId = null;
 			}
+
 			assert.deepEqual( handlerWasCalled, true, "OCDoResource handler was called" );
+
 			testUtils.testShutdown();
+
 			done();
 		};
 
 	if ( testUtils.testStartup( iotivity.OCMode.OC_CLIENT ) ===
 			iotivity.OCStackResult.OC_STACK_OK ) {
 
-		stopProcessing = testUtils.testProcessing( 100, function() {
-			teardown();
-		} );
+		stopTestServer = testUtils.startTestServer(
+			function() {
+				stopProcessing = testUtils.testProcessing( 100, function() {
+					teardown();
+				} );
 
-		failsafeTimeoutId = setTimeout( function() {
-			failsafeTimeoutId = null;
-			teardown();
-		}, 5000 );
+				failsafeTimeoutId = setTimeout( function() {
+					failsafeTimeoutId = null;
+					teardown();
+				}, 5000 );
 
-		result = iotivity.OCDoResource(
-			handle,
-			iotivity.OCMethod.OC_REST_GET,
-			"light",
-			null,
-			null,
-			iotivity.OCConnectivityType.OC_ALL,
-			iotivity.OCQualityOfService.OC_HIGH_QOS,
-			function( handle, response ) {
-				handlerWasCalled = true;
-				teardown();
+				result = iotivity.OCDoResource(
+					handle,
+					iotivity.OCMethod.OC_REST_GET,
+					"light",
+					null,
+					null,
+					iotivity.OCConnectivityType.OC_ALL,
+					iotivity.OCQualityOfService.OC_HIGH_QOS,
+					function( handle, response ) {
+						handlerWasCalled = true;
+						teardown();
+					},
+					null,
+					0 );
 			},
-			null,
-			0 );
+			function() {
+				teardown();
+			} );
 	}
 } );
