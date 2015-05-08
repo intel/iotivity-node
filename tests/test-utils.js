80ede57c509eba2c99c720e0a88522f18156118c
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-05-08 12:28:17 +0300

    Tests: Use underscore to render test utils more legible

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,4 +1,5 @@
-var testUtils = function( iotivity, assert ) {
+var _ = require( "underscore" ),
+	testUtils = function( iotivity, assert ) {
 	if ( !this._isTestUtils ) {
 		return new testUtils( iotivity, assert );
 	}
@@ -6,60 +7,62 @@ var testUtils = function( iotivity, assert ) {
 	this._iotivity = iotivity;
 	this._assert = assert;
 };
-testUtils.prototype._isTestUtils = true;
 
-testUtils.prototype.testStartup = function( mode ) {
-	var result = this._iotivity.OCInit( null, 0, mode );
+_.extend( testUtils.prototype, {
+	_isTestUtils: true,
 
-	this._assert.deepEqual(
-		this.lookupEnumValueName( "OCStackResult", result ),
-		"OC_STACK_OK",
-		"OCInit in " + this.lookupEnumValueName( "OCMode", mode ) + " mode has succeeded" );
+	testStartup: function( mode ) {
+		var result = this._iotivity.OCInit( null, 0, mode );
 
-	return result;
-};
-
-testUtils.prototype.testProcessing = function( interval, teardown ) {
-	var that = this,
-		callCount = 0,
-		result = that._iotivity.OCStackResult.OC_STACK_OK,
-		id = setInterval( function() {
-			result = that._iotivity.OCProcess();
-			if ( result === that._iotivity.OCStackResult.OC_STACK_OK ) {
-				callCount++;
-			} else {
-				teardown();
-			}
-		}, interval );
-
-	return function stopProcessing() {
-		clearInterval( id );
-		that._assert.deepEqual(
-			that.lookupEnumValueName( "OCStackResult", result ),
+		this._assert.deepEqual(
+			this.lookupEnumValueName( "OCStackResult", result ),
 			"OC_STACK_OK",
-			"OCProcess has not failed once in " + callCount + " calls" );
-	};
-};
+			"OCInit in " + this.lookupEnumValueName( "OCMode", mode ) + " mode has succeeded" );
 
-testUtils.prototype.testShutdown = function() {
-	var result = this._iotivity.OCStop();
+		return result;
+	},
 
-	this._assert.deepEqual(
-		this.lookupEnumValueName( "OCStackResult", result ),
-		"OC_STACK_OK",
-		"OCStop has succeeded" );
-};
+	testProcessing: function( interval, teardown ) {
+		var callCount = 0,
+			result = this._iotivity.OCStackResult.OC_STACK_OK,
+			id = setInterval( _.bind( function() {
+				result = this._iotivity.OCProcess();
+				if ( result === this._iotivity.OCStackResult.OC_STACK_OK ) {
+					callCount++;
+				} else {
+					teardown();
+				}
+			}, this ), interval );
 
-testUtils.prototype.lookupEnumValueName = function( enumName, value ) {
-	var index,
-		enumeration = this._iotivity[ enumName ];
+		return _.bind( function stopProcessing() {
+			clearInterval( id );
+			this._assert.deepEqual(
+				this.lookupEnumValueName( "OCStackResult", result ),
+				"OC_STACK_OK",
+				"OCProcess has not failed once in " + callCount + " calls" );
+		}, this );
+	},
 
-	for ( index in enumeration ) {
-		if ( enumeration[ index ] === value ) {
-			return index;
+	testShutdown: function() {
+		var result = this._iotivity.OCStop();
+
+		this._assert.deepEqual(
+			this.lookupEnumValueName( "OCStackResult", result ),
+			"OC_STACK_OK",
+			"OCStop has succeeded" );
+	},
+
+	lookupEnumValueName: function( enumName, value ) {
+		var index,
+			enumeration = this._iotivity[ enumName ];
+
+		for ( index in enumeration ) {
+			if ( enumeration[ index ] === value ) {
+				return index;
+			}
 		}
 	}
-}
 
+} );
 
 module.exports = testUtils;
