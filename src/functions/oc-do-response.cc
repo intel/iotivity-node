2e0480c6e8312c248d995ff6af24c7324884f416
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-05-13 10:56:34 +0300

    Functions: Forgot to add OCDoResponse() files

---
+++ src/functions/oc-do-response.cc
@@ -0,0 +1,27 @@
+#include <node_buffer.h>
+#include "oc-do-resource.h"
+#include "../typechecks.h"
+#include "../structures.h"
+
+extern "C" {
+#include <stdlib.h>
+#include <ocstack.h>
+}
+
+using namespace v8;
+using namespace node;
+
+void bind_OCDoResponse( const FunctionCallbackInfo<Value>& args ) {
+	Isolate *isolate = Isolate::GetCurrent();
+	OCEntityHandlerResponse response;
+	char payload[ MAX_RESPONSE_LENGTH ];
+
+	response.payload = payload;
+
+	VALIDATE_ARGUMENT_COUNT( isolate, args, 1 );
+	VALIDATE_ARGUMENT_TYPE( isolate, args, 0, IsObject );
+
+	if ( c_OCEntityHandlerResponse( &response, args[ 0 ]->ToObject() ) ) {
+		args->GetReturnValue()->Set( Number::New( OCDoResponse( &response ) ) );
+	}
+}
---
+++ src/functions/oc-do-response.h
@@ -0,0 +1,8 @@
+#ifndef __IOTIVITY_NODE_OCDORESPONSE_H__
+#define __IOTIVITY_NODE_OCDORESPONSE_H__
+
+#include <v8.h>
+
+void bind_OCDoResponse( const v8::FunctionCallbackInfo<v8::Value>& args );
+
+#endif /* __IOTIVITY_NODE_OCDORESPONSE_H__ */
