a40631d825c1cb4d3ecbddc5fe59fec756bad8a2
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-05-17 02:49:12 +0300

    Build: Write dist script

---
+++ dist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p dist/iotivity
+
+npm install && npm test || ( echo "Repair build first."; exit 1; )
+
+npm prune --production
+
+cp -a AUTHORS.txt index.js MIT-LICENSE.txt node_modules README.md dist/iotivity
+mkdir -p dist/iotivity/build/Release
+cp build/Release/iotivity.node dist/iotivity/build/Release
+
+cd dist
+tar cvjf iotivity.tar.bz2 iotivity
+cd ..
+
+rm -rf dist/iotivity
+
+npm install
