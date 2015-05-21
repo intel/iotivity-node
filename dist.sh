bd3dfbcb3d8428e95fa457c961c9120f8e6d4ea9
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-05-21 20:26:24 +0300

    Build: dist.sh stops on error

--- dist.sh
+++ dist.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-rm -rf dist && mkdir -p dist/iotivity
+rm -rf dist && \
+mkdir -p dist/iotivity &&
+npm install
 
-npm install && npm test || ( echo "Repair build first."; exit 1; )
+if ! npm test; then
+	echo "Repair build first."
+	exit 1
+fi
 
-npm prune --production
-
-cp -a AUTHORS.txt index.js MIT-LICENSE.txt node_modules README.md dist/iotivity
-mkdir -p dist/iotivity/build/Release
-cp build/Release/iotivity.node dist/iotivity/build/Release
-
-cd dist
-tar cvjf iotivity.tar.bz2 iotivity
-cd ..
-
+npm prune --production &&
+cp -a AUTHORS.txt index.js MIT-LICENSE.txt node_modules README.md dist/iotivity &&
+mkdir -p dist/iotivity/build/Release &&
+cp build/Release/iotivity.node dist/iotivity/build/Release &&
+cd dist &&
+tar cvjf iotivity.tar.bz2 iotivity &&
+cd .. &&
 npm install
