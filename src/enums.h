77677c5fe3d8a1a8610a0fd198aee48187599774
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-06-05 16:20:43 +0300

    Constants: Update to master and introduce legibility macro

--- src/enums.h
+++ src/enums.h
@@ -8,4 +8,10 @@ using namespace v8;
 
 void InitEnums(Handle<Object> exports);
 
+#define SET_CONSTANT_MEMBER(destination, name)                      \
+  (destination)                                                 \
+      ->ForceSet(NanNew<String>(#name), NanNew<Number>((name)), \
+                 static_cast<PropertyAttribute>(ReadOnly || DontDelete));
+
+
 #endif /* __IOTIVITY_NODE_ENUMS_H__ */
--- src/main.cc
+++ src/main.cc
@@ -14,17 +14,17 @@ void Init(Handle<Object> exports, Handle<Object> module) {
   InitFunctions(exports, module);
   InitEnums(exports);
 
-  exports->Set(NanNew<String>("MAX_RESPONSE_LENGTH"),
-               NanNew<Number>(MAX_RESPONSE_LENGTH));
-
-  exports->Set(NanNew<String>("MAX_URI_LENGTH"),
-               NanNew<Number>(MAX_URI_LENGTH));
-
-  exports->Set(NanNew<String>("MAX_HEADER_OPTIONS"),
-               NanNew<Number>(MAX_HEADER_OPTIONS));
-
-  exports->Set(NanNew<String>("MAX_HEADER_OPTION_DATA_LENGTH"),
-               NanNew<Number>(MAX_HEADER_OPTION_DATA_LENGTH));
+  SET_CONSTANT_MEMBER( exports, DEV_ADDR_SIZE_MAX );
+  SET_CONSTANT_MEMBER( exports, MAX_RESPONSE_LENGTH );
+  SET_CONSTANT_MEMBER( exports, MAX_REQUEST_LENGTH );
+  SET_CONSTANT_MEMBER( exports, MAX_URI_LENGTH );
+  SET_CONSTANT_MEMBER( exports, MAX_QUERY_LENGTH );
+  SET_CONSTANT_MEMBER( exports, MAX_MANUFACTURER_NAME_LENGTH );
+  SET_CONSTANT_MEMBER( exports, MAX_MANUFACTURER_URL_LENGTH );
+  SET_CONSTANT_MEMBER( exports, MAX_CONTAINED_RESOURCES );
+  SET_CONSTANT_MEMBER( exports, MAX_HEADER_OPTIONS );
+  SET_CONSTANT_MEMBER( exports, MAX_HEADER_OPTION_DATA_LENGTH );
+  SET_CONSTANT_MEMBER( exports, MAX_CB_TIMEOUT_SECONDS );
 }
 
 NODE_MODULE(iotivity, Init)
