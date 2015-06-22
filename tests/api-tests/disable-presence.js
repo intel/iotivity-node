4db473b8352b0118c6765d2b31a9db1986d89997
Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>
2015-06-22 17:12:06 +0530

    JS API: Implement enablePresence and disablePresence. Added test cases for the apis.

---
+++ tests/api-tests/disable-presence.js
@@ -0,0 +1,25 @@
+require( "../setup" );
+
+var OicDevice = require( "../../index" );
+
+test( "Enable Presence", function( assert ) {
+	var done = assert.async();
+
+	expect( 1 );
+
+  var device = OicDevice();
+  var settings = {
+    role: "server"
+  }
+  device.configure( settings );
+  device._server.enablePresence( 1000 );
+  device._server.disablePresence().then(
+		function() {
+			ok( true, "Presence Disabled successfully" );
+			done();
+		},
+		function( error ) {
+			ok( false, "Presence disabled error with code: " + error.result );
+			done();
+		} );      
+} );
