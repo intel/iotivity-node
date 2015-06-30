14af1d2e724c29e49c4e49be82e6d3e341ab8759
Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>
2015-06-30 17:18:38 +0530

    JS API: Use the this.listener. Fixes errors.

--- index.js
+++ index.js
@@ -128,28 +128,28 @@ _.extend( OicDevice.prototype, {
 		listeners: {},
 
 		addEventListener: function ( event, callback ) {
-			if ( !( event in listeners ) ) {
-				listeners[ event ] = [];
+			if ( !( event in this.listeners ) ) {
+				this.listeners[ event ] = [];
 			}
 			// Currently allows duplicate callbacks. Should it?
-			listeners[ event ].push( callback );
+			this.listeners[ event ].push( callback );
 		},
 
 		removeEventListener: function ( event, callback ) {
-			if ( event in listeners ) {
-				listeners [ event ] = listeners [ event ].filter ( function ( ev ) {
+			if ( event in this.listeners ) {
+				this.listeners [ event ] = this.listeners [ event ].filter ( function ( ev ) {
 					return ev !== callback;
 				});
 			}
 		},
 
 		dispatchEvent: function ( event, request ) {
-			if ( typeof self [ "on" + event ] === "function" ) {
-				self [ "on" + event ] ( request );
+			if ( typeof this [ "on" + event ] === "function" ) {
+				this [ "on" + event ] ( request );
 			}
-			if ( event in listeners ) {
-				for ( var i = 0, len = listeners [ event ].length; i < len; i++ ) {
-					listeners [ event ] [ i ].call( self, request );
+			if ( event in this.listeners ) {
+				for ( var i = 0, len = this.listeners [ event ].length; i < len; i++ ) {
+					this.listeners [ event ] [ i ].call( this, request );
 				}
 			}
 		},
