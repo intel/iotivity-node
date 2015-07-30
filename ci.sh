edc4f6206afe7dc6561f93ab0b8bcf558487f165
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-07-30 13:31:40 +0300

    Build: Add CI

---
+++ ci.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -x
+
+get_output_path() {
+	scons -h | \
+	awk \
+		-v 'release=""' \
+		-v 'os=""' \
+		-v 'arch=""' \
+		-v 'context=""' \
+		'
+		{
+			if ( $1 == "RELEASE:" ) {
+				context = "release";
+			} else if ( $1 == "TARGET_OS:" ) {
+				context = "os";
+			} else if ( $1 == "TARGET_ARCH:" ) {
+				context = "arch";
+			}
+
+			if ( $1 == "actual:" ) {
+				if ( context == "release" ) {
+					release = ( $2 == "True" ? "release" : "debug" );
+					context = "";
+				} else if ( context == "os" ) {
+					os = $2;
+					context = "";
+				} else if ( context == "arch" ) {
+					arch = $2;
+					context = "";
+				}
+			}
+		}
+		END {
+			print( "out/" os "/" arch "/" release );
+		}
+	'
+}
+
+rm -rf depbuild || exit 1
+mkdir -p ./depbuild || exit 1
+
+# Download and build iotivity from tarball
+cd ./depbuild || exit 1
+	wget https://downloads.iotivity.org/0.9.1/iotivity-0.9.1.tar.gz || exit 1
+	tar xzf iotivity-0.9.1.tar.gz || exit 1
+
+	cd iotivity-0.9.1 || exit 1
+		IOTIVITY_PATH="$( pwd )"
+		OUTPUT_PATH="${IOTIVITY_PATH}/$( get_output_path )"
+		test "x${OUTPUT_PATH}x" = "xx" && exit 1
+		scons liboctbstack || { cat config.log; exit 1; }
+
+cd ../../ || exit 1
+
+# Run ./dist.sh
+OCTBSTACK_CFLAGS="-I${IOTIVITY_PATH}/resource/csdk/stack/include" \
+OCTBSTACK_LIBS="-loctbstack -L${OUTPUT_PATH} -Wl,-rpath ${OUTPUT_PATH}" \
+./dist.sh || exit 1
