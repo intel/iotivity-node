3a51a8420b76ebe90ae355a75e46a4c63fb01a34
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-08-07 21:56:05 +0300

    All: Creating a resource works again

---
+++ js/server.discoverable.js
@@ -0,0 +1,39 @@
+var intervalId,
+	handle = {},
+	iotivity = require( "iotivity" );
+
+// Start iotivity and set up the processing loop
+iotivity.OCInit( null, 0, iotivity.OCMode.OC_SERVER );
+
+intervalId = setInterval( function() {
+	iotivity.OCProcess();
+}, 1000 );
+
+// Create a new resource
+iotivity.OCCreateResource(
+
+	// The bindings fill in this object
+	handle,
+
+	"core.fan",
+	iotivity.OC_RSRVD_INTERFACE_DEFAULT,
+	"/a/fan",
+	function( flag, request ) {
+		console.log( "Entity handler called with flag = " + flag + " and the following request:" );
+		console.log( JSON.stringify( request, null, 4 ) );
+		return iotivity.OCEntityHandlerResult.OC_EH_OK;
+	},
+	iotivity.OCResourceProperty.OC_DISCOVERABLE );
+
+
+// Exit gracefully when interrupted
+process.on( "SIGINT", function() {
+	console.log( "SIGINT: Quitting..." );
+
+	// Tear down the processing loop and stop iotivity
+	clearInterval( intervalId );
+	iotivity.OCStop();
+
+	// Exit
+	process.exit( 0 );
+} );
