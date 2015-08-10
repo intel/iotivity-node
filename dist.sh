dbbfb1f12a84ebe0856fa7c884713ee2724c1157
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-08-10 14:33:32 +0300

    Build: Only copy deps if deps is present

--- dist.sh
+++ dist.sh
@@ -27,8 +27,10 @@ npm prune --production &&
 cp -a AUTHORS.txt index.js MIT-LICENSE.txt node_modules README.md dist/iotivity &&
 mkdir -p dist/iotivity/build/Release &&
 cp build/Release/iotivity.node dist/iotivity/build/Release &&
-mkdir -p dist/iotivity/deps/iotivity/lib
-cp deps/iotivity/lib/liboctbstack.so dist/iotivity/deps/iotivity/lib
+if test -d deps; then
+	mkdir -p dist/iotivity/deps/iotivity/lib
+	cp deps/iotivity/lib/liboctbstack.so dist/iotivity/deps/iotivity/lib
+fi
 cd dist &&
 tar cvjf iotivity.tar.bz2 iotivity &&
 cd .. &&
