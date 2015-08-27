f593a47c9f480540edd0dac93bf14275ef84f744
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-08-27 13:18:00 +0300

    JS Apps: Add presence client/server example

---
+++ js/client.presence.js
@@ -0,0 +1,78 @@
+var intervalId,
+	handle = {},
+
+	// This is the same value as server.presence.js
+	sampleUri = "/a/iotivity-node-presence-sample",
+	iotivity = require( "iotivity" );
+
+// Start iotivity and set up the processing loop
+iotivity.OCInit( null, 0, iotivity.OCMode.OC_CLIENT );
+
+intervalId = setInterval( function() {
+	iotivity.OCProcess();
+}, 1000 );
+
+// Discover resources and list them
+iotivity.OCDoResource(
+
+	// The bindings fill in this object
+	handle,
+
+	iotivity.OCMethod.OC_REST_DISCOVER,
+
+	// Standard path for discovering resources
+	iotivity.OC_RSRVD_WELL_KNOWN_URI,
+
+	// There is no destination
+	null,
+
+	// There is no payload
+	null,
+	iotivity.OCConnectivityType.CT_DEFAULT,
+	iotivity.OCQualityOfService.OC_HIGH_QOS,
+	function( handle, response ) {
+		console.log( "Received response to DISCOVER request:" );
+		console.log( JSON.stringify( response, null, 4 ) );
+		var index,
+			destination = response.addr,
+			presenceHandle = {},
+			resources = response && response.payload && response.payload.resources,
+			resourceCount = resources.length ? resources.length : 0;
+
+		// If the sample URI is among the resources, issue the GET request to it
+		for ( index = 0 ; index < resourceCount ; index++ ) {
+			if ( resources[ index ].uri === sampleUri ) {
+				iotivity.OCDoResource(
+					presenceHandle,
+					iotivity.OCMethod.OC_REST_PRESENCE,
+					iotivity.OC_RSRVD_PRESENCE_URI,
+					destination,
+					null,
+					iotivity.OCConnectivityType.CT_DEFAULT,
+					iotivity.OCQualityOfService.OC_HIGH_QOS,
+					function( handle, response ) {
+						console.log( "Received response to PRESENCE request:" );
+						console.log( JSON.stringify( response, null, 4 ) );
+						return iotivity.OCStackApplicationResult.OC_STACK_KEEP_TRANSACTION;
+					},
+					null );
+			}
+		}
+
+		return iotivity.OCStackApplicationResult.OC_STACK_KEEP_TRANSACTION;
+	},
+
+	// There are no header options
+	null );
+
+// Exit gracefully when interrupted
+process.on( "SIGINT", function() {
+	console.log( "SIGINT: Quitting..." );
+
+	// Tear down the processing loop and stop iotivity
+	clearInterval( intervalId );
+	iotivity.OCStop();
+
+	// Exit
+	process.exit( 0 );
+} );
---
+++ js/server.presence.js
@@ -0,0 +1,52 @@
+var intervalId,
+	handle = {},
+
+	// This is the same value as client.presence.js
+	sampleUri = "/a/iotivity-node-presence-sample",
+	iotivity = require( "iotivity" );
+
+// Start iotivity and set up the processing loop
+iotivity.OCInit( null, 0, iotivity.OCMode.OC_SERVER );
+
+intervalId = setInterval( function() {
+	iotivity.OCProcess();
+}, 1000 );
+
+iotivity.OCSetDeviceInfo( { deviceName: "server.discoverable" } );
+iotivity.OCSetPlatformInfo( {
+	platformID: "server.discoverable.sample",
+	manufacturerName: "iotivity-node"
+} );
+
+iotivity.OCStartPresence( 0 );
+
+// Create a new resource
+iotivity.OCCreateResource(
+
+	// The bindings fill in this object
+	handle,
+
+	"core.fan",
+	iotivity.OC_RSRVD_INTERFACE_DEFAULT,
+	sampleUri,
+	function( flag, request ) {
+		console.log( "Entity handler called with flag = " + flag + " and the following request:" );
+		console.log( JSON.stringify( request, null, 4 ) );
+		return iotivity.OCEntityHandlerResult.OC_EH_OK;
+	},
+	iotivity.OCResourceProperty.OC_DISCOVERABLE );
+
+
+// Exit gracefully when interrupted
+process.on( "SIGINT", function() {
+	console.log( "SIGINT: Quitting..." );
+
+	// Tear down the processing loop and stop iotivity
+	clearInterval( intervalId );
+	iotivity.OCDeleteResource( handle.handle );
+	iotivity.OCStopPresence();
+	iotivity.OCStop();
+
+	// Exit
+	process.exit( 0 );
+} );
