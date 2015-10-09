6ff8cb059e73eedd4770d94d08b99993d9293d40
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-10-09 12:25:57 +0300

    CI: Run basic tests in build-only mode

--- grunt-build/tasks/testsuite.js
+++ grunt-build/tasks/testsuite.js
@@ -5,7 +5,10 @@ var path = require( "path" ),
 
 grunt.task.registerTask( "testsuite", "Run the test suite", function() {
 	var done = this.async();
-	spawn( "node", [ path.join( process.cwd(), "tests", "suite.js" ) ], { stdio: "inherit" } )
+	spawn( "node",
+		[ path.join( process.cwd(), "tests", "suite.js" ) ]
+			.concat( grunt.option( "suites" ) ? [ grunt.option( "suites" ) ] : [] ),
+		{ stdio: "inherit" } )
 		.on( "close", function( code ) {
 			done( code === 0 || code === null );
 		} );
