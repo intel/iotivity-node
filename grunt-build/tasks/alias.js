5f8d7ec83891d6f7a7d77071d1fc8170ae6e55f7
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-10-09 11:48:09 +0300

    Build: Factor out linting into task "lint"

--- grunt-build/tasks/alias.js
+++ grunt-build/tasks/alias.js
@@ -1,8 +1,10 @@
 module.exports = function( grunt ) {
 
+grunt.registerTask( "lint", [ "jshint", "jscs" ] );
+
 grunt.registerTask( "default", [ "test" ] );
 
-grunt.registerTask( "test", [ "jshint", "jscs", "testsuite", "testdist" ] );
+grunt.registerTask( "test", [ "lint", "testsuite", "testdist" ] );
 
 grunt.registerTask( "format", [ "esformatter", "clangformat" ] );
 
