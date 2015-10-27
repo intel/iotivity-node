5657b8dfa899c4a4d857afe7c54be997e3d66b52
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-10-27 10:59:53 +0200

    Build: Remove formatting test from lint

--- grunt-build/tasks/alias.js
+++ grunt-build/tasks/alias.js
@@ -1,6 +1,6 @@
 module.exports = function( grunt ) {
 
-grunt.registerTask( "lint", [ "jshint", "jscs", "testformat" ] );
+grunt.registerTask( "lint", [ "jshint", "jscs" ] );
 
 grunt.registerTask( "default", [ "test" ] );
 
