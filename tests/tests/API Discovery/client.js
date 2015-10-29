ade73d7d2f756def3b7482ecac82c953cdd0e275
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-10-29 22:26:51 +0200

    Tests: Test resource deduplication via API Discovery

--- tests/tests/API Discovery/client.js
+++ tests/tests/API Discovery/client.js
@@ -1,49 +1,87 @@
-var resourceFound = false,
-	quitConditionCount = 0,
+var _ = require( "lodash" ),
+	async = require( "async" ),
 	utils = require( "../../assert-to-console" ),
 	device = require( "../../../index" )(),
 	uuid = process.argv[ 2 ];
 
-console.log( JSON.stringify( { assertionCount: 3 } ) );
+console.log( JSON.stringify( { assertionCount: 6 } ) );
 
-function maybeQuit() {
-	quitConditionCount++;
+function discoverTheResource( callback ) {
+	var eventHandler = function( event ) {
+			var index,
+				count = 0,
+				url = "/a/" + uuid;
 
-	if ( quitConditionCount >= 2 ) {
-		console.log( JSON.stringify( { killPeer: true } ) );
-		process.exit( 0 );
-	}
-}
-
-device.configure( {
-	role: "client",
-	connectionMode: "acked"
-} ).then(
-	function() {
-		utils.assert( "ok", true, "Client: device.configure() successful" );
-
-		device.client.addEventListener( "resourcefound", function( event ) {
-
-			if ( !resourceFound && event.resource.uri === "/a/" + uuid ) {
-				resourceFound = true;
+			if ( event.resource.url === url ) {
 				utils.assert( "ok", true, "Client: Resource found" );
 
-				// We found the resource, so we have completed one step towards concluding the test
-				maybeQuit();
+				for ( index in device.client._resources ) {
+					if ( device.client._resources[ index ].url === url ) {
+						count++;
+					}
+				}
+
+				utils.assert( "strictEqual", count, 1,
+					"Client: Resource present exactly once among resources" );
+
+				maybeDone();
 			}
-		} );
+		},
+		maybeDoneCount = 0,
+		maybeDone = function( error ) {
+			var doCallback = false;
+			if ( error ) {
+				doCallback = true;
+			} else {
+				maybeDoneCount++;
+				if ( maybeDoneCount >= 2 ) {
+					doCallback = true;
+				}
+			}
+			if ( doCallback ) {
+				device.client.removeEventListener( "resourcefound", eventHandler );
+				callback( error );
+			}
+		};
+
+		device.client.addEventListener( "resourcefound", eventHandler );
 
 		device.client.findResources().then(
 			function() {
-				utils.assert( "ok", true, "Client: findResources() successful" );
-				maybeQuit();
+				utils.assert( "ok", true, "Client: device.client.findResources() successful" );
+				maybeDone();
 			},
 			function( error ) {
-				utils.die( "Client: findResources() failed with: " + error + " and result " +
-					error.result );
+				maybeDone( _.extend( error, { step: "device.client.findResources()" } ) );
+			} );
+}
+
+async.series( [
+
+	// Configure the device
+	function( callback ) {
+		device.configure( {
+			role: "client",
+			connectionMode: "acked"
+		} ).then(
+			function() {
+				callback( null );
+			},
+			function( error ) {
+				callback( _.extend( error, { step: "device.configure()" } ) );
 			} );
 	},
-	function( error ) {
-		utils.die( "Client: device.configure() failed with: " + error );
-	} );
 
+	// Discover the resource twice
+	discoverTheResource,
+	discoverTheResource
+
+], function( error ) {
+	if ( error ) {
+		utils.die( "Client: " + error.step + " failed with " + error + " and result " +
+			error.result );
+	} else {
+		console.log( JSON.stringify( { killPeer: true } ) );
+		process.exit( 0 );
+	}
+} );
