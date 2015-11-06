6831caf404b029c7b41de4ee54835fb0ffbf8f08
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-11-06 14:24:33 +0200

    Examples: Add high-level create/delete example

---
+++ js/high-level-resource-creation-client.js
@@ -0,0 +1,56 @@
+var device = require( "iotivity-node" )();
+
+function throwError( error ) {
+	console.error( error.stack ? error.stack : ( error.message ? error.message : error ) );
+	process.exit( 1 );
+}
+
+device.configure( {
+	role: "client",
+	connectionMode: "acked"
+} ).then(
+	function() {
+
+		new Promise( function( fulfill, reject ) {
+
+			var resourceFoundHandler = function( event ) {
+				console.log( "Discovered resource(s) via the following event:\n" +
+					JSON.stringify( event, null, 4 ) );
+
+				// We've discovered the resource we were seeking.
+				if ( event.resource.uri === "/a/high-level-resource-creation-example" ) {
+					console.log( "Found the test server" );
+					device.client.removeEventListener( "resourcefound", resourceFoundHandler );
+					fulfill( event.resource.deviceId );
+				}
+			};
+
+			// Add a listener that will receive the results of the discovery
+			device.client.addEventListener( "resourcefound", resourceFoundHandler );
+
+			device.client.findResources().catch( function( error ) {
+				device.client.removeEventListener( "resourcefound", resourceFoundHandler );
+				reject( "findResource() failed: " + error );
+			} );
+		} ).then(
+			function( deviceId ) {
+				console.log( "deviceId discovered" );
+				device.client.createResource( {
+					deviceId: deviceId,
+					url: "/a/new-resource",
+					resourceTypes: [ "core.light" ],
+					interfaces: [ "oic.if.baseline" ],
+					properties: {
+						exampleProperty: 23
+					}
+				} ).then(
+					function( resource ) {
+						console.log( "remote resource successfully created with id " +
+							resource.id );
+						device.client.deleteResource( resource.id ).then( function() {
+							console.log( "remote resource successfully deleted" );
+						}, throwError );
+					},
+					throwError );
+			}, throwError );
+	}, throwError );
---
+++ js/high-level-resource-creation-server.js
@@ -0,0 +1,64 @@
+var resourceCreatedByRemote,
+	device = require( "iotivity-node" )(),
+	_ = require( "lodash" ),
+	settings = {
+		role: "server",
+		connectionMode: "acked",
+		info: {
+			uuid: "INTEL-server",
+			name: "api-server-example",
+			manufacturerName: "Intel",
+			manufactureDate: "Wed Sep 23 10:04:17 EEST 2015",
+			platformVersion: "0.9.2",
+			firmwareVersion: "0.0.1",
+			supportUrl: "http://example.com/"
+		}
+	};
+
+function throwError( error ) {
+	console.error( error.stack ? error.stack : ( error.message ? error.message : error ) );
+	process.exit( 1 );
+}
+
+device.configure( settings ).then(
+	function() {
+		device.server.addEventListener( "request", function( request ) {
+			console.log( JSON.stringify( request ), null, 4 );
+			if ( request.type === "create" ) {
+				console.log( "create request" );
+				device.server.registerResource( _.extend( request.res, {
+					discoverable: true
+				} ) ).then( function( resource ) {
+					console.log( "resource successfully registered" );
+					resourceCreatedByRemote = resource;
+					request.sendResponse( null );
+				}, _.bind( request.sendError, request ) );
+				return;
+			} else if ( request.type === "delete" ) {
+				console.log( "delete request" );
+				if ( request.target === resourceCreatedByRemote.id ) {
+					device.server.unregisterResource( resourceCreatedByRemote.id ).then(
+						function() {
+							console.log( "resource successfully deleted" );
+							request.sendResponse( null );
+						},
+						_.bind( request.sendError, request ) );
+				}
+			}
+		} );
+
+		if ( device.settings.info.uuid ) {
+			device.server.registerResource( {
+				url: "/a/high-level-resource-creation-example",
+				deviceId: device.settings.info.uuid,
+				connectionMode: device.settings.connectionMode,
+				resourceTypes: [ "core.light" ],
+				interfaces: [ "oic.if.baseline" ],
+				discoverable: true,
+				observable: true,
+				properties: { someValue: 0, someOtherValue: "Helsinki" }
+			} ).then( function() {
+				console.log( "initial resource successfully registered" );
+			}, throwError );
+		}
+	}, throwError );
