8bee1f816c444daaec06af4fbb7a2cc1e52ed46c
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-11-16 22:37:15 +0200

    OicClient: Resolve discovery promise upon first response
    Reject previous discovery upon initiation of new discovery

--- tests/tests/API No Resources.js
+++ tests/tests/API No Resources.js
@@ -2,24 +2,22 @@ var device = require( "../../index" )(),
 	utils = require( "../assert-to-console" ),
 	iotivity = require( "../../lowlevel" );
 
-console.log( JSON.stringify( { assertionCount: 1 } ) );
+console.log( JSON.stringify( { assertionCount: 2 } ) );
 
 device.configure( {
 	role: "client",
 	connectionMode: "acked"
 } ).then(
 	function() {
-		try {
-			utils.assert( "strictEqual",
-				device.client._processFindResourceResponse( null,
-					{ result: iotivity.OCStackResult.OC_STACK_NO_RESOURCE } ),
-				iotivity.OCStackApplicationResult.OC_STACK_KEEP_TRANSACTION,
-				"_processFindResourceRequest() handles OC_STACK_NO_RESOURCE" );
-			process.exit( 0 );
-		} catch ( error ) {
-			utils.assert( "strictEqual", error, undefined,
-				"_processFindResourceRequest throws no errors" );
-		}
+		utils.assert( "strictEqual",
+			device.client._processResources( null,
+				{ result: iotivity.OCStackResult.OC_STACK_NO_RESOURCE },
+				function() {
+					utils.assert( "ok", true, "_processResources() fulfills promise" );
+				}, function() {} ),
+			iotivity.OCStackApplicationResult.OC_STACK_KEEP_TRANSACTION,
+			"_processResources() handles OC_STACK_NO_RESOURCE" );
+		process.exit( 0 );
 	},
 	function() {
 		utils.die( "device.configure() failed" );
