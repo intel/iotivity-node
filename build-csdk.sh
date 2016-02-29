b2917eeefcecd45f728fd53b368b9656ad85bb99
Gabriel Schulhof <gabriel.schulhof@intel.com>
2016-02-29 15:53:56 +0200

    All: Update AUTHORS.txt and move license to Apache 2.0

--- build-csdk.sh
+++ build-csdk.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Use node to parse package.json and extract the upstream version from the package version. This
 # assumes the package version is of the form major.minor.patch[-dev-commitid]. It grabs whatever
 # follows a zero-or-more-length-series of tokens that end in a dash.
