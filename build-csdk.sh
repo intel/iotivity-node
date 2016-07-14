ffed9f311985afa6ea5e43bdd5e9b5370907ea03
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2016-07-14 10:54:45 +0300

    build-csdk: Checkout the working revision of tinycbor
    IoTivity 1.1.0 doesn't compile with the tinycbor from master,
    so checkout the working revision of tinycbor.
    
    Closes gh-62
    
    Change-Id: Id024087a644ba8b69044c409a1ced2dfed412f37
    Signed-off-by: Sudarsana Nagineni <sudarsana.nagineni@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/9497
    Reviewed-by: Gabriel Schulhof <gabriel.schulhof@intel.com>
    Tested-by: Gabriel Schulhof <gabriel.schulhof@intel.com>

--- build-csdk.sh
+++ build-csdk.sh
@@ -77,6 +77,9 @@ if test "x${DO_BUILD}x" = "xtruex"; then
 
 			# iotivity wants us to clone this before it'll do anything
 			git clone https://github.com/01org/tinycbor.git extlibs/tinycbor/tinycbor
+			cd extlibs/tinycbor/tinycbor
+			git checkout dbc0129400f22087d4be3396cc44ea922d2f07f8
+			cd -
 			scons $SCONS_FLAGS liboctbstack libconnectivity_abstraction libcoap c_common libocsrm routingmanager || { cat config.log; exit 1; }
 
 	cd ../../ || exit 1
