7c2edf1868cbddbe9eb5eb20c38c2230854e7684
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-10-06 15:39:47 +0300

    Samples: Add coaps example and update provisioning

---
+++ js/client.get.coaps.js
@@ -0,0 +1,121 @@
+// Copyright 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var intervalId,
+	handleReceptacle = {},
+
+	// This is the same value as server.get.js
+	sampleUri = "/a/iotivity-node-get-sample",
+	iotivity = require( "iotivity-node/lowlevel" );
+
+console.log( "Starting OCF stack in client mode" );
+
+iotivity.OCRegisterPersistentStorageHandler( require( "../lib/StorageHandler" )() );
+
+// Start iotivity and set up the processing loop
+iotivity.OCInit( null, 0, iotivity.OCMode.OC_CLIENT_SERVER );
+
+intervalId = setInterval( function() {
+	iotivity.OCProcess();
+}, 1000 );
+
+console.log( "Issuing discovery request" );
+
+// Discover resources and list them
+iotivity.OCDoResource(
+
+	// The bindings fill in this object
+	handleReceptacle,
+
+	iotivity.OCMethod.OC_REST_DISCOVER,
+
+	// Standard path for discovering resources
+	iotivity.OC_MULTICAST_DISCOVERY_URI,
+
+	// There is no destination
+	null,
+
+	// There is no payload
+	null,
+	iotivity.OCConnectivityType.CT_DEFAULT,
+	iotivity.OCQualityOfService.OC_HIGH_QOS,
+	function( handle, response ) {
+		console.log( "Received response to DISCOVER request:" );
+		console.log( JSON.stringify( response, null, 4 ) );
+		var index,
+			destination = response.addr,
+			getHandleReceptacle = {},
+			resources = response && response.payload && response.payload.resources,
+			resourceCount = resources ? resources.length : 0,
+			getResponseHandler = function( handle, response ) {
+				console.log( "Received response to GET request:" );
+				console.log( JSON.stringify( response, null, 4 ) );
+				return iotivity.OCStackApplicationResult.OC_STACK_DELETE_TRANSACTION;
+			};
+
+		// If the sample URI is among the resources, issue the GET request to it
+		for ( index = 0; index < resourceCount; index++ ) {
+			if ( resources[ index ].uri === sampleUri ) {
+				iotivity.OCDoResource(
+					getHandleReceptacle,
+					iotivity.OCMethod.OC_REST_GET,
+					( function() {
+						var endpoint;
+						var endpointIndex;
+						var result;
+						for ( endpointIndex in resources[ index ].eps ) {
+							endpoint = resources[ index ].eps[ endpointIndex ];
+							if ( endpoint.tps  === "coaps" ) {
+								result = ( endpoint.tps + "://" +
+									( endpoint.family & iotivity.OCTransportFlags.OC_IP_USE_V6 ? "[" : "" ) +
+									endpoint.addr.replace( /[%].*$/, "" ) +
+									( endpoint.family & iotivity.OCTransportFlags.OC_IP_USE_V6 ? "]" : "" ) +
+									":" + endpoint.port ) + sampleUri;
+								console.log( "GET request to " + result );
+								return result;
+							}
+						}
+						throw new Error( "No secure endpoint found!" );
+					})(),
+					null,
+					{
+						type: iotivity.OCPayloadType.PAYLOAD_TYPE_REPRESENTATION,
+						values: {
+							question: "How many angels can dance on the head of a pin?"
+						}
+					},
+					iotivity.OCConnectivityType.CT_DEFAULT,
+					iotivity.OCQualityOfService.OC_HIGH_QOS,
+					getResponseHandler,
+					null );
+			}
+		}
+
+		return iotivity.OCStackApplicationResult.OC_STACK_KEEP_TRANSACTION;
+	},
+
+	// There are no header options
+	null );
+
+// Exit gracefully when interrupted
+process.on( "SIGINT", function() {
+	console.log( "SIGINT: Quitting..." );
+
+	// Tear down the processing loop and stop iotivity
+	clearInterval( intervalId );
+	iotivity.OCStop();
+
+	// Exit
+	process.exit( 0 );
+} );
---
+++ js/server.get.coaps.js
@@ -0,0 +1,98 @@
+// Copyright 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var intervalId,
+	handleReceptacle = {},
+
+	// This is the same value as server.get.js
+	sampleUri = "/a/iotivity-node-get-sample",
+	iotivity = require( "iotivity-node/lowlevel" );
+
+console.log( "Starting OCF stack in server mode" );
+
+iotivity.OCRegisterPersistentStorageHandler( require( "../lib/StorageHandler" )() );
+
+// Start iotivity and set up the processing loop
+iotivity.OCInit( null, 0, iotivity.OCMode.OC_SERVER );
+
+iotivity.OCSetPropertyValue( iotivity.OCPayloadType.PAYLOAD_TYPE_DEVICE,
+	iotivity.OC_RSRVD_SPEC_VERSION, "res.1.1.0" );
+iotivity.OCSetPropertyValue( iotivity.OCPayloadType.PAYLOAD_TYPE_DEVICE,
+	iotivity.OC_RSRVD_DATA_MODEL_VERSION, "abc.0.0.1" );
+iotivity.OCSetPropertyValue( iotivity.OCPayloadType.PAYLOAD_TYPE_DEVICE,
+	iotivity.OC_RSRVD_DEVICE_NAME, "server.get" );
+
+iotivity.OCSetPropertyValue( iotivity.OCPayloadType.PAYLOAD_TYPE_PLATFORM,
+	iotivity.OC_RSRVD_MFG_NAME, "iotivity-node" );
+
+intervalId = setInterval( function() {
+	iotivity.OCProcess();
+}, 1000 );
+
+console.log( "Registering resource" );
+
+// Create a new resource
+iotivity.OCCreateResource(
+
+	// The bindings fill in this object
+	handleReceptacle,
+
+	"core.fan",
+	iotivity.OC_RSRVD_INTERFACE_DEFAULT,
+	sampleUri,
+	function( flag, request ) {
+		console.log( "Entity handler called with flag = " + flag + " and the following request:" );
+		console.log( JSON.stringify( request, null, 4 ) );
+
+		// If we find the magic question in the request, we return the magic answer
+		if ( request && request.payload && request.payload.values &&
+				request.payload.values.question ===
+				"How many angels can dance on the head of a pin?" ) {
+
+			iotivity.OCDoResponse( {
+				requestHandle: request.requestHandle,
+				resourceHandle: request.resource,
+				ehResult: iotivity.OCEntityHandlerResult.OC_EH_OK,
+				payload: {
+					type: iotivity.OCPayloadType.PAYLOAD_TYPE_REPRESENTATION,
+					values: {
+						"answer": "As many as wanting."
+					}
+				},
+				resourceUri: sampleUri,
+				sendVendorSpecificHeaderOptions: []
+			} );
+
+			return iotivity.OCEntityHandlerResult.OC_EH_OK;
+		}
+
+		// By default we error out
+		return iotivity.OCEntityHandlerResult.OC_EH_ERROR;
+	},
+	iotivity.OCResourceProperty.OC_DISCOVERABLE | iotivity.OCResourceProperty.OC_SECURE );
+
+console.log( "Server ready" );
+
+// Exit gracefully when interrupted
+process.on( "SIGINT", function() {
+	console.log( "SIGINT: Quitting..." );
+
+	// Tear down the processing loop and stop iotivity
+	clearInterval( intervalId );
+	iotivity.OCDeleteResource( handleReceptacle.handle );
+	iotivity.OCStop();
+
+	// Exit
+	process.exit( 0 );
+} );
